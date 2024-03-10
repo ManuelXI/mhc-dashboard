4b1ac44fe49145a07a3000d180c876f454418ef9
Emmanuel Mortey <exmortey@gmail.com>
2024-03-10 16:10:57 +0000

    Worked on Top Professionals, Updated Top Bookings, Top Sport Places

--- src/app/dashboard/bookings/top-pros/top-pros.component.ts
+++ src/app/dashboard/bookings/top-pros/top-pros.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { TableData } from '../../../models/types.type';
 
 @Component({
   selector: 'app-top-pros',
   templateUrl: './top-pros.component.html',
-  styleUrl: './top-pros.component.scss'
+  styleUrl: './top-pros.component.scss',
 })
 export class TopProsComponent {
-
+  items: TableData[] = [
+    {
+      name: 'Raymond Richards',
+      amount: 2500,
+      imageUrl: 'assets/dashboard/Top Professionals/Albert Flores.svg',
+    },
+    {
+      name: 'Arlene McCoy',
+      amount: 2000,
+      imageUrl: 'assets/dashboard/Top Professionals/Arlene McCoy.svg',
+    },
+    {
+      name: 'Diane Russel',
+      amount: 1500,
+      imageUrl: 'assets/dashboard/Top Professionals/Diane Russel.svg',
+    },
+    {
+      name: 'Albert Flores',
+      amount: 1000,
+      imageUrl: 'assets/dashboard/Top Professionals/Albert Flores.svg',
+    },
+    {
+      name: 'James Williams',
+      amount: 500,
+      imageUrl: 'assets/dashboard/Top Professionals/James Williams.svg',
+    },
+  ];
 }
--- src/app/models/types.type.ts
+++ src/app/models/types.type.ts
@@ -27,4 +27,5 @@ export interface Data {
 export interface TableData {
   name: string;
   amount: number;
+  imageUrl?: string;
 }
