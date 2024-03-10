5e74c4905249410f289455930b360ca21b792080
Emmanuel Mortey <exmortey@gmail.com>
2024-03-10 16:37:54 +0000

    Worked on Top Income

--- src/app/dashboard/finances/top-income/top-income.component.ts
+++ src/app/dashboard/finances/top-income/top-income.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TableData } from '../../../models/types.type';
 
 @Component({
   selector: 'app-top-income',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './top-income.component.scss'
 })
 export class TopIncomeComponent {
-
+  items: TableData[] = [
+    {
+      name: 'Ava Greendale',
+      amount: 12200,
+      imageUrl: 'assets/dashboard/Top Income/Ava Greendale.svg',
+    },
+    {
+      name: 'Ben Robertsson',
+      amount: 11758,
+      imageUrl: 'assets/dashboard/Top Income/Ben Robertsson.svg',
+    },
+    {
+      name: 'Cole McCallen',
+      amount: 10342,
+      imageUrl: 'assets/dashboard/Top Income/Cole McCallen.svg',
+    },
+    {
+      name: 'Davon Parker',
+      amount: 10103,
+      imageUrl: 'assets/dashboard/Top Income/Davon Parker.svg',
+    },
+    {
+      name: 'Elisa Mellory',
+      amount: 9209,
+      imageUrl: 'assets/dashboard/Top Income/Elisa Mellory.svg',
+    },
+  ];
 }
