eea779ab90b4df801afd113b51dc5e6d5781103f
Emmanuel Mortey <exmortey@gmail.com>
2024-03-10 15:43:32 +0000

    Worked on Top Sport Places

--- src/app/dashboard/bookings/top-sports-places/top-sports-places.component.ts
+++ src/app/dashboard/bookings/top-sports-places/top-sports-places.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TableData } from '../../../models/types.type';
 
 @Component({
   selector: 'app-top-sports-places',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './top-sports-places.component.scss'
 })
 export class TopSportsPlacesComponent {
-
+  items: TableData[] = [
+    { name: 'Halo Salt Gym', amount: 849 },
+    { name: 'Dream Sweam Pool', amount: 624 },
+    { name: 'Energytherapy', amount: 453 },
+    { name: 'YoGa House', amount: 374 },
+    { name: 'Wellness Gym', amount: 294 }
+  ];  
 }
