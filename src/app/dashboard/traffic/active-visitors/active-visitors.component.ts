ebb4663d488d712b0d87dd8f31ccc9da2c996400
Emmanuel Mortey <exmortey@gmail.com>
2024-03-10 18:45:22 +0000

    Completed Work on Active Visitors

--- src/app/dashboard/traffic/active-visitors/active-visitors.component.ts
+++ src/app/dashboard/traffic/active-visitors/active-visitors.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Data } from '../../../models/types.type';
+import Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-active-visitors',
@@ -6,5 +8,79 @@ import { Component } from '@angular/core';
   styleUrl: './active-visitors.component.scss'
 })
 export class ActiveVisitorsComponent {
+  highcharts = Highcharts;
+  pieChartData: Data[] = [
+    {
+      name: 'Mobile',
+      amount: 78,
+      color: '#0085C8',
+    },
+    {
+      name: 'Desktop',
+      amount: 14,
+      color: '#DC5EB2',
+    },
+    {
+      name: 'Tablet',
+      amount: 8,
+      color: '#DEE049',
+    },
+  ];
+  pieChartOptions: Highcharts.Options = {
+    chart: {
+      plotShadow: false,
+    },
+    title: {
+      text: undefined,
+    },
+    tooltip: {
+      pointFormat: '<b>{point.percentage:.1f}%</b>',
+    },
+    plotOptions: {
+      pie: {
+        shadow: false,
+        size: '100%',
+        // innerSize: '75%',
+        dataLabels: {
+          enabled: false,
+        },
+      },
+    },
+    series: [
+      {
+        type: 'pie',
+        data: [
+          {
+            name: 'Mobile',
+            y: 78,
+            color: '#0085C8',
+          },
+          {
+            name: 'Desktop',
+            y: 14,
+            color: '#DC5EB2',
+          },
+          {
+            name: 'Tablet',
+            y: 8,
+            color: '#DEE049',
+          },
+        ],
+      },
+    ],
+    credits: {
+      enabled: false,
+    },
+  };
 
+  getTotalAmount(): number {
+    let totalAmount = 0;
+    const data = this.pieChartData;
+
+    for (const item of data) {
+      totalAmount += item.amount;
+    }
+
+    return totalAmount;
+  }
 }
